748833d1a0976600fc495cfab6e02113d74953dd
Samuel Magalhães <samuelmagalhaes@id.uff.br>
2021-05-23 00:30:36 -0300

    first commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Switch C&V</title>
+        <link href="<?php echo base_url()?>assets/css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Switch C&V</h3></div>
+                                    <div class="card-body">
+                                        <div class="small mb-3 text-muted"></div>
+                                        <form action="<?php echo base_url();?>index.php/welcome/gerartxt" method="post">
+                                            <div class="form-group">
+                                                <label class="small mb-1" for="inputEmailAddress">número da loja</label>
+                                                <input class="form-control py-4" id="textochave" name="textochave" type="text"  placeholder="número" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label class="small mb-1" for="inputEmailAddress">nome da loja</label>
+                                                <input class="form-control py-4" id="textochave1" name="textochave1" type="text"  placeholder="nome" />
+                                            </div>
+                                            <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <button class="btn btn-primary" type="submit ">Gerar</a>
+                                            </div>
+                                        </form>
+                                    </div>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="<?php echo base_url()?>assets/js/scripts.js"></script>
+    </body>
+</html>
